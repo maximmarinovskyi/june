709035463d60616a0f8bd5ea64d78250cd73e796
maximmarinovskyi <91957078+maximmarinovskyi@users.noreply.github.com>
2023-06-01 16:36:20 +0300

    1

--- src/Fam.js
+++ src/Fam.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, RouteLink } from 'react-router-dom';
+import "./Fam.css"
 
 class Fam extends React.Component {
   render() {
     return (
-
       <div className="Fam">
-        <p>Family page page !!!</p>
-      </div>
+        <ul>
+          <li><a href="#">Maxim</a></li>
+          <li><a href="#">Olga</a></li>
+          <li><a href="#">Maria</a></li>
+          <li><a href="#">Andrei</a></li>
 
+        </ul>
+
+
+      </div>
     );
   }
 }
